e65f06768414465342dc6fa822b30dd8f521a602
ChamikaRohan <chamika707@gmail.com>
2024-06-04 23:56:04 +0530

    Removed sensitive contents for security reasons

--- server/src/main/java/com/server/server/ServerApplication.java
+++ server/src/main/java/com/server/server/ServerApplication.java
@@ -2,12 +2,22 @@ package com.server.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class ServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ServerApplication.class, args);
+		//SpringApplication.run(ServerApplication.class, args);
+
+		ConfigurableApplicationContext context = SpringApplication.run(ServerApplication.class, args);
+		Environment environment = context.getEnvironment();
+
+        // Retrieving and printing the values of environment variables
+        String mongoUri = environment.getProperty("SPRING_DATA_MONGODB_URI");
+
+        System.out.println("MongoDB URI: " + mongoUri);
 	}
 
 }
